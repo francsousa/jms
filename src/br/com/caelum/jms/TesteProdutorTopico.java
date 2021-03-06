c1347c3da1ca7eb0d5ad208e207363a1f4dca674
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-03-06 17:15:57 -0300

    Tópicos e assinaturas duráveis

--- src/br/com/caelum/jms/TesteProdutorTopico.java
+++ src/br/com/caelum/jms/TesteProdutorTopico.java
@@ -8,29 +8,24 @@ import javax.jms.MessageProducer;
 import javax.jms.Session;
 import javax.naming.InitialContext;
 
-public class TesteProdutor {
+public class TesteProdutorTopico {
 
 	public static void main(String[] args) throws Exception {
-		
+
 		InitialContext context = new InitialContext();
 		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
-		
+
 		Connection connection = factory.createConnection();
 		connection.start();
 		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		
-		Destination fila = (Destination) context.lookup("financeiro");
-		
-		MessageProducer producer = session.createProducer(fila);
-		
-		for (int i = 0; i < 1000; i++) {
-			
-			Message message = session.createTextMessage("<pedido><id>" + i + "</id></pedido>");
-			producer.send(message);
-		}
-		
-		// new Scanner(System.in).nextLine();
-		
+
+		Destination topico = (Destination) context.lookup("loja");
+
+		MessageProducer producer = session.createProducer(topico);
+
+		Message message = session.createTextMessage("<pedido><id>1011</id></pedido>");
+		producer.send(message);
+
 		session.close();
 		connection.close();
 		context.close();
