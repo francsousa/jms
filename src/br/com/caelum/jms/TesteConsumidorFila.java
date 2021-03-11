c3a6dcd1daaf417c95aa6c6c3afb24a6a6b408ac
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-03-11 08:49:53 -0300

    Usando acknowledge

--- src/br/com/caelum/jms/TesteConsumidorFila.java
+++ src/br/com/caelum/jms/TesteConsumidorFila.java
@@ -23,7 +23,7 @@ public class TesteConsumidorFila {
 		
 		Connection connection = factory.createConnection();
 		connection.start();
-		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Session session = connection.createSession(true, Session.SESSION_TRANSACTED);
 		
 		Destination fila = (Destination) context.lookup("financeiro");
 		MessageConsumer consumer = session.createConsumer(fila);
@@ -36,7 +36,9 @@ public class TesteConsumidorFila {
 				TextMessage textMessage = (TextMessage) message;
 				
 				try {
+					// message.acknowledge();
 					System.out.println(textMessage.getText());
+					session.rollback();
 				} catch (JMSException e) {
 					e.printStackTrace();
 				}
--- src/br/com/caelum/jms/TesteProdutorFila.java
+++ src/br/com/caelum/jms/TesteProdutorFila.java
@@ -23,11 +23,14 @@ public class TesteProdutorFila {
 		
 		MessageProducer producer = session.createProducer(fila);
 		
-		for (int i = 0; i < 1000; i++) {
-			
-			Message message = session.createTextMessage("<pedido><id>" + i + "</id></pedido>");
-			producer.send(message);
-		}
+		Message message = session.createTextMessage("<pedido><id>13</id></pedido>");
+		producer.send(message);
+		
+//		for (int i = 0; i < 1000; i++) {
+//			
+//			Message message = session.createTextMessage("<pedido><id>" + i + "</id></pedido>");
+//			producer.send(message);
+//		}
 		
 		// new Scanner(System.in).nextLine();
 		
