cbb1f12edfee4f2e2f4323f984530d9e283a071b
Francisco Sousa <franciscoeds1987@hotmail.com>
2021-03-11 10:02:17 -0300

    Prioridade e Tempo de vida da mensagem

---
+++ src/br/com/caelum/jms/log/TesteProdutorFilaLog.java
@@ -0,0 +1,42 @@
+package br.com.caelum.jms.log;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.naming.InitialContext;
+
+public class TesteProdutorFilaLog {
+
+	public static void main(String[] args) throws Exception {
+		
+		InitialContext context = new InitialContext();
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		
+		Connection connection = factory.createConnection();
+		connection.start();
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		Destination fila = (Destination) context.lookup("LOG");
+		
+		MessageProducer producer = session.createProducer(fila);
+		
+		Message message = session.createTextMessage("WARN | Starting Jetty server");
+		producer.send(message, DeliveryMode.NON_PERSISTENT, 7, 80000);
+		
+//		for (int i = 0; i < 1000; i++) {
+//			
+//			Message message = session.createTextMessage("<pedido><id>" + i + "</id></pedido>");
+//			producer.send(message);
+//		}
+		
+		// new Scanner(System.in).nextLine();
+		
+		session.close();
+		connection.close();
+		context.close();
+	}
+}
